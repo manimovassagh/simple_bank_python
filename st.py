b1d0b9dec07189776cf73eea9031e4d8c4505339
Mani Movassagh <mani.mgh@gmail.com>
2024-02-03 00:40:51 +0100

    print

--- st.py
+++ st.py
@@ -7,6 +7,14 @@ class User:
         print("Some printing")
 
         User.print_me()
+        User.print_me()
+        User.print_me()
+        User.print_me()
+        User.print_me()
+        User.print_me()
+        User.print_me()
+        User.print_me()
+        User.print_me()
     @staticmethod
     def print_me():
         print("Some printing")
