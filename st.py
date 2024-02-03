e0932c3b800e7ef6416c17c0350e585fc61c52b1
Mani Movassagh <mani.mgh@gmail.com>
2024-02-03 18:29:26 +0100

    d

--- st.py
+++ st.py
@@ -27,7 +27,6 @@ class User:
     @staticmethod
     def print_me():
         print("Some printing")
-    def print_me():
         print("Some printing")
         print("Some printing")
         print("Some printing")
