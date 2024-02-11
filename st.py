a3102b696c6d78ec864f8ff8bcda6de3910ed455
Mani Movassagh <mani.mgh@gmail.com>
2024-02-11 01:37:05 +0100

    add

--- st.py
+++ st.py
@@ -6,4 +6,25 @@ class User:
     def print_me():
         print("Some printing")
         User.print_me()
+    @staticmethod
+    def print_me():
+        print("Some printing")
+        User.print_me()
+    @staticmethod
+    def print_me():
+        print("Some printing")
+        User.print_me()
+    @staticmethod
+    def print_me():
+        print("Some printing")
+        User.print_me()
+    @staticmethod
+    def print_me():
+        print("Some printing")
+        User.print_me()
+    @staticmethod
+    def print_me():
+        print("Some printing")
+        User.print_me()
+
 
